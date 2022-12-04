1eefeef67c02e056b5886164104b93e556831f5f
babinnepali <babinnepalibtl@gmail.com>
2022-12-04 01:43:59 -0500

    added docs

--- nodepro/server.js
+++ nodepro/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const PORT = 4000;
 const server = http.createServer((req,res)=>{
-    res.write("Hello first node!");
+    res.write("Hello first");
     res.end();
 });
 
