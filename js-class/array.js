29e223810e09d3711e75c97f8fb21e8b82fd606a
babinnepali <babinnepalibtl@gmail.com>
2022-12-04 23:48:47 -0500

    added array

---
+++ js-class/array.js
@@ -0,0 +1,7 @@
+//It is a special non primitive datatypes
+// Array literal
+// variable_type identifier = [element1, element2, element3]
+
+const fruits = ['apple', 'banana', 'cherry', age, 1998-2022];
+
+const cars = newArray('BMW', 'Toyota', 'Porsche')
