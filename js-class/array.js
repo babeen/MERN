44dbbbe1988b01768e15ce736858fab6dc1a251d
babinnepali <babinnepalibtl@gmail.com>
2022-12-05 00:09:53 -0500

    added array

--- js-class/array.js
+++ js-class/array.js
@@ -1,7 +1,20 @@
 //It is a special non primitive datatypes
 // Array literal
 // variable_type identifier = [element1, element2, element3]
+const age = 19;
+age = 20;
+console.log(age);
+/*
+const fruits = ['apple', 'banana', 'cherry', 'age', 1998-2022];
 
-const fruits = ['apple', 'banana', 'cherry', age, 1998-2022];
+// const cars = newArray('BMW', 'Toyota', 'Porsche')
 
-const cars = newArray('BMW', 'Toyota', 'Porsche')
+const friends = []
+friends[0] = 'Babin';
+friends[1] = 'Manish';
+friends[2] = 'Bikash';
+friends[3] = 'Dipesh';
+
+friends[1]='Miraj';
+console.log(friends);
+*/
