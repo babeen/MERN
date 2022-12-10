3f568a8424f9541dc4dae8440fd0d97b1c447561
babinnepali <babinnepalibtl@gmail.com>
2022-12-09 22:42:05 -0500

    added server locally withoud port

--- nodepro/app.js
+++ nodepro/app.js
@@ -1,7 +1,11 @@
 const express =require("express");
 const app = express();
+const PORT = 4000;
 app.get("/",(req,res) => {
     res.json("Hello Framework");
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log("server");
+})
+
